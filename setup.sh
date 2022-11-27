74c53d4f8a544408571f1e776343e94c180c9243
ANILINDORE <107453981+ANILINDORE@users.noreply.github.com>
2022-11-27 16:32:36 +0530

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"srivastavaanil.india@gmail.com\"\n\
+email = \"21f1004374@student.onlinedegree.iitm.ac.in\"\n\
 "> ~/.streamlit/credentials.toml
 
 echo "\
